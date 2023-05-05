bc229c0057cadcece19d8920bbbd49fed5202d30
silvio009 <RM550821@fiap.com.br>
2023-05-04 22:52:53 -0300

    formulario

--- js/script.js
+++ js/script.js
@@ -20,31 +20,11 @@
 // console.log(mn.textContent);
 
 //Recuperando uma collection de elementos...
-const aElements = document.getElementsByTagName("a");
-const aElementsArray = [...aElements];
-
-aElementsArray.forEach((a)=>{
-    console.log(a);
-});
-
+//
 //Exercícios
 //1 - Recupere uma coleção de imagens da página e imprima o
 // atributo src...
-const imgElements = [...document.getElementsByTagName("img")];
-// const imgElementsArray = [...imgElements];
-// console.log(imgElements);
-
-//IMPRIMINDO O ATRIBUTO SRC do elemento com o forEach
-imgElements.forEach((img)=>{
-    console.log(`Path das imagens : ${img.src}`);
-});
-
-const botao = document.querySelector("div > button");
-
-botao.addEventListener("click", ()=>{
-    alert("TCHAU!!!");
-});
-
+//
 
 
 // //Declarando um array em Javascript
@@ -61,3 +41,55 @@ botao.addEventListener("click", ()=>{
 // nr3.forEach( (numero)=>{
 //     console.log("ITEM DO ARRAY : " + numero);
 // } );
+
+//et frutas = ["laranja","banana", "acerola", "abacate", "caqui", "uva"]
+ // inserir itens ao final do array com o metodo do array;
+ //rutas.push("melancia")
+ //console.log(frutas)
+  // inicio do array 
+//frutas.unshift("kiwi")
+ //console.log(frutas)
+ //removendo item do array
+ //frutas.pop("kiwi")
+ //onsole.log(frutas)
+ // remover do inicio do array
+ //frutas.shift("laranja")
+ //console.log(frutas)
+
+// realizando a busca de um item no array usando o metodo indexof(iten)
+// quando usamos o indexOf (iten) ele retorna o indice do item procurado 
+//indice = frutas.indexOf("acerola")
+//console.log(frutas[indice])
+
+// utilizando  o metodo splice(indice do item, qnt de vezes que ele será rermovido ) para remover um item do array em conjunto com o indexof
+//et indice = frutas.indexOf("abacate")
+//console.log("fruta que esta no index antes da remoção: " + frutas[indice])
+//frutas.splice(indice,1)
+//console.log("fruta que ficou no index depois da remoção: " + frutas[indice])
+//console.table(frutas)
+
+
+
+
+let frutas = ["laranja","banana", "acerola", "abacate", "caqui", "uva"]
+const aElements = document.querySelectorAll("a")
+//loop for
+//for(let x = 0; x < frutas.length ; x++ ){
+//    console.log("valor de  x  :" + x );
+//}
+
+//for (const fruta of frutas){    // melhor//
+ //   console.log(fruta)
+//}
+//for (const indice in frutas){
+ //   console.log("frutas:" + frutas [indice])
+// }
+//frutas.map((fruta,key)=>{
+    //console.log('fruta nr ${key} ${fruta}');
+
+//});
+const inputUser = document.querySelector("#idUser");
+inputUser.addEventListener("focus", ()=>{
+    inputUser.setAttribute("style", "outline-color:#ff0000;");
+
+})
